a1c834856cc4fab0a5dd12fd0b04e43de77be1f2
Raphaelcouto16 <raphaelcouto99@gmail.com>
2024-05-01 21:54:00 -0300

    Update Apriori.py

--- Apriori.py
+++ Apriori.py
@@ -11,10 +11,10 @@ conn = psycopg2.connect(
 )
 
 # Carregar eventos_df da tabela de eventos do banco de dados
-eventos_df = pd.read_sql_query('SELECT nome_do_evento, area, sobre FROM eventos', conn)
+eventos_df = pd.read_sql_query('SELECT id_evento ,nome_do_evento, area, sobre FROM eventos', conn)
 
 # Carregar cursos_df da tabela de cursos do banco de dados
-cursos_df = pd.read_sql_query('SELECT nome, area FROM cursos', conn)
+cursos_df = pd.read_sql_query('SELECT id_curso, nome, area FROM cursos', conn)
 
 # Defina a limiar de similaridade
 limiar_similaridade = 55  # Você pode ajustar esse valor conforme necessário
@@ -35,18 +35,19 @@ for index, evento in eventos_df.iterrows():
     if not cursos_filtrados.empty:
         print(f"Cursos associados para o evento '{nome_evento}':")
         print(cursos_filtrados)
-        #laço nos cursos filtrados e inserindo no banco o id do curso e do evento
+        # laço nos cursos filtrados e inserindo no banco o id do curso e do evento
         for id_curso, curso in cursos_filtrados.iterrows():
             cursor = conn.cursor()
-            print(id_curso)
+            id_curso_real = curso['id_curso']  # Obtenha o ID real do curso
+            print(id_curso_real)
             print(index)
-            cursor.execute("INSERT INTO cursos_eventos (id_curso, id_evento) VALUES (%s, %s)", (id_curso, index))
+            cursor.execute("INSERT INTO cursos_eventos (id_curso, id_evento) VALUES (%s, %s)", (id_curso_real, evento['id_evento']))
             conn.commit()
             cursor.close()
         eventos_encontrados += 1
     else:
         print(f"Não foram encontrados cursos associados para o evento '{nome_evento}' com quantidade de caracteres próximos ou iguais.")
-        eventos_nao_encontrados  += 1
+        eventos_nao_encontrados += 1
 
 print(f"\nTotal de eventos: {len(eventos_df)}")
 print(f"Total de cursos: {len(cursos_df)}")
@@ -68,4 +69,9 @@ print(f"Porcentagem de eventos nao encontrados: {porcentagem_eventos_nao_encontr
 #	CONSTRAINT crusos_eventos_pkey PRIMARY KEY (id_curso_evento)
 #)
 
-
+#monte uma consulta ligando eventos e cursos com a cursos_eventos em sql 
+#SELECT * FROM eventos
+#INNER JOIN cursos_eventos ON eventos.id_evento = cursos_eventos.id_evento
+#INNER JOIN cursos ON cursos_eventos.id_curso = cursos.id_curso
+#WHERE eventos.id_evento = 1
+#ORDER BY cursos.nome ASC
